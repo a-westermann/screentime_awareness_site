1db9d9ec0f7f0934bdbdaf226018b21c1d04ad5e
Vierce1 <a.westermann.19@gmail.com>
2024-09-18 16:21:14 -0500

    reading in db info

--- screentime_awareness/helpers/security.py
+++ screentime_awareness/helpers/security.py
@@ -10,6 +10,8 @@ def encrypt_pw(user_id: str, password: str):
     # add the local secret to the password and hash them
     salt = bcrypt.gensalt()
     hashed_pw = str(bcrypt.hashpw((get_secret() + password).encode(), salt))
+    print(f'salt: {salt}')
+    print(f'password hashed: {hashed_pw}')
     dbc = DBC()
     sql = f"insert into users values('{user_id}', '{hashed_pw}');"
     dbc.write(sql)
