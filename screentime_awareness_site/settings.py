6e923c40141eae823d47de6685a94a5d342c84af
Vierce1 <a.westermann.19@gmail.com>
2024-09-18 14:18:55 -0500

    media root

--- screentime_awareness_site/settings.py
+++ screentime_awareness_site/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -74,7 +74,8 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'screentime_awareness_site.wsgi.application'
 
-MEDIA_ROOT = r"home/andweste/screentime_awareness_site/screentime_awareness_site/media/"
+MEDIA_ROOT = os.path.join(os.curdir, 'media')
+    #"home/andweste/screentime_awareness_site/screentime_awareness_site/media/"
 MEDIA_URL = "/media/"
 
 # Database
