755b90d16c65573daa77002e13b34461363c5a00
Vierce1 <a.westermann.19@gmail.com>
2024-09-18 14:16:25 -0500

    media root

--- screentime_awareness_site/settings.py
+++ screentime_awareness_site/settings.py
@@ -74,7 +74,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'screentime_awareness_site.wsgi.application'
 
-MEDIA_ROOT = r"home/screentime_awareness_site/screentime_awareness_site/media/"
+MEDIA_ROOT = r"home/andweste/screentime_awareness_site/screentime_awareness_site/media/"
 MEDIA_URL = "/media/"
 
 # Database
