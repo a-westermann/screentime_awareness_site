80aa76bfe7f21eeca4079d45ca9a00894e8cc2ad
Vierce1 <a.westermann.19@gmail.com>
2024-09-18 16:05:30 -0500

    reading in db info

--- screentime_awareness/helpers/db.py
+++ screentime_awareness/helpers/db.py
@@ -4,7 +4,7 @@ import json
 
 class DBC:
     def __init__(self):
-        with open('screentime_awarenss/helpers/db.json') as file:
+        with open('screentime_awareness/helpers/db.json') as file:
             self.db_info = json.load(file)
             print(self.db_info)
             print(self.db_info['database'])
