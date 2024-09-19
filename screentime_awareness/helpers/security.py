b175444fda8eb87ae4f93a5d37a80777bc86a35c
Vierce1 <a.westermann.19@gmail.com>
2024-09-19 11:35:15 -0500

    reading in db info

--- screentime_awareness/helpers/security.py
+++ screentime_awareness/helpers/security.py
@@ -21,4 +21,5 @@ def validate_pw(user_id: str, entered_pw: str) -> bool:
     dbc = DBC()
     sql = f"select * from users where id = '{user_id}' LIMIT 1;"
     hashed_pw = dbc.select(sql)[0]['hashed_password']
-    return bcrypt.checkpw(hashed_pw,  str.encode(entered_pw))
+    entered_pw =  entered_pw.encode()
+    return bcrypt.checkpw(hashed_pw, entered_pw)
