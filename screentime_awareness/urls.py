2268ad7f8053331936799837acc7f73e08a2018a
Vierce1 <a.westermann.19@gmail.com>
2024-09-19 12:13:17 -0500

    log in form

--- screentime_awareness/urls.py
+++ screentime_awareness/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('', views.log_in, name='log_in')
+    path('', views.log_in, name='log_in'),
     path('learnmore', views.learn_more, name='learn_more'),
     path('donate', views.donate, name='donate')
 ]
