8d82a07cc7c7c63412090458f14d8320bdc361ea
Vierce1 <a.westermann.19@gmail.com>
2024-09-20 14:38:22 -0500

    log in and registration page

--- screentime_awareness/helpers/security.py
+++ screentime_awareness/helpers/security.py
@@ -10,7 +10,7 @@ def add_user_to_db(user_id: str, password: str):
     # add the local secret to the password and hash them
     salt = bcrypt.gensalt()
     # note that using bcrypt, the salt is saved into the hash itself, so no need to store separately
-    hashed_pw = bcrypt.hashpw((get_secret() + password).encode(), salt).decode()
+    hashed_pw = bcrypt.hashpw((password + get_secret()).encode(), salt).decode()
     print(f'salt: {salt}')
     print(f'password hashed: {hashed_pw}')
     dbc = DBC()
