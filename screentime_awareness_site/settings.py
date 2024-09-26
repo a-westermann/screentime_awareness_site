7611d20eafaeb11c9c0458c52f18e9cd61baef88
Vierce1 <a.westermann.19@gmail.com>
2024-09-26 15:37:06 -0500

    settings

--- screentime_awareness_site/settings.py
+++ screentime_awareness_site/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-by2q8kl9a&tb_!)-r_*0s1#w_74_+1mzkyhn7qqi1_19^gmx_d')
 DEBUG = os.environ.get('DJANGO_SECRET_KEY') is None
-# SECURE_SSL_REDIRECT = True
+SECURE_SSL_REDIRECT = False  # Change
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 SECURE_HSTS_SECONDS = 15
