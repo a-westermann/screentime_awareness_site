51281d9bae84f566e7a672b0bea06f022d652f8c
Vierce1 <a.westermann.19@gmail.com>
2024-09-26 12:15:37 -0500

    build settings

--- screentime_awareness_site/settings.py
+++ screentime_awareness_site/settings.py
@@ -20,10 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-by2q8kl9a&tb_!)-r_*0s1#w_74_+1mzkyhn7qqi1_19^gmx_d'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-by2q8kl9a&tb_!)-r_*0s1#w_74_+1mzkyhn7qqi1_19^gmx_d')
+DEBUG = os.environ.get('DJANGO_SECRET_KEY') is None
 
 ALLOWED_HOSTS = ['52.15.62.202',  # gobeyondthescreen.org aws ec2 instance
                  '192.168.1.31',
