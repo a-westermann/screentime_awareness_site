e1a16818e582168d50be0aa0ee2c8f888d1ad68c
Vierce1 <a.westermann.19@gmail.com>
2024-09-26 13:51:26 -0500

    wsgi

--- wsgi.py
+++ wsgi.py
@@ -2,6 +2,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 # Ensure you set this to your settings module, e.g., 'myproject.settings'
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'screentime_awareness_site.settings.py')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'screentime_awareness_site/settings.py')
 
 application = get_wsgi_application()
