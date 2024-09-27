43d053abe0b58d71eb5784973fb31cd7a779e6c2
Vierce1 <a.westermann.19@gmail.com>
2024-09-27 13:39:41 -0500

    static files fixes

--- screentime_awareness_site/settings.py
+++ screentime_awareness_site/settings.py
@@ -134,7 +134,7 @@ SESSION_COOKIE_AGE = 30 * 60  # expire session after 30 minutes of no new cookie
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static',
+STATICFILES_DIRS = ['static',
                     ]
 
 # Default primary key field type
