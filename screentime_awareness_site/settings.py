234e878cc4eb3d66fd1e3284f5bb53f934ee248e
Vierce1 <a.westermann.19@gmail.com>
2024-09-27 14:51:05 -0500

    static files fixes

--- screentime_awareness_site/settings.py
+++ screentime_awareness_site/settings.py
@@ -133,9 +133,9 @@ SESSION_COOKIE_AGE = 30 * 60  # expire session after 30 minutes of no new cookie
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 # STATIC_ROOT = 'home/ubuntu/screentime_awareness_site/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, '/static/'),
+STATIC_ROOT = '/home/ubuntu/screentime_awareness_site/static/'
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR/'static/',
                     ]
 
 # Default primary key field type
