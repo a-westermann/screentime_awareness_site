95cc992284c72cd8aea4cb26d7f64842b0e47e87
Vierce1 <a.westermann.19@gmail.com>
2025-03-16 14:45:05 -0500

    dnd shops

--- screentime_awareness/views.py
+++ screentime_awareness/views.py
@@ -164,7 +164,7 @@ def donate(request):
 # Could I just have 1 view that looks up the shop name based on the url?
 def ember(request):
     dbc = db.DBC()
-    # inventory = dbc.select("select * from shop_inventories where shop = 'ember&ink'")
+    inventory = dbc.select("select * from shop_inventories where shop = 'ember&ink'")
     context = {'items': 'hello'
                #    inventory
                }
