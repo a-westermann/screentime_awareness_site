8ead124cbd43686e9b22f00611c773c2c2e5853f
Vierce1 <a.westermann.19@gmail.com>
2025-03-17 12:48:10 -0500

    dnd shops

--- screentime_awareness/views.py
+++ screentime_awareness/views.py
@@ -167,7 +167,7 @@ def shop(request):
     inventory = dbc.select(
         f"select * from shop_inventories where shop = '{shop_name}' order by special_text, item_name;", )
     context = {'items': inventory,
-               'shop_name': shop_name}
+               'shop_name': shop_name.replace('_', ' ')}
     return render(request, 'dnd/shop.html', context=context)
 
 # def ember(request):
