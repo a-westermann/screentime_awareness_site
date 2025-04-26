0be09f0c340a7b16ef7f2e174f0e0c1bc8d087c1
Vierce1 <a.westermann.19@gmail.com>
2025-04-26 14:07:54 -0500

    static root

--- screentime_awareness_site/settings.py
+++ screentime_awareness_site/settings.py
@@ -143,6 +143,8 @@ SESSION_COOKIE_AGE = 30 * 60  # expire session after 30 minutes of no new cookie
 # Static Root is not really needed. collectstatic is used to collect all static files from
 # 3rd party libraries, etc into the folder that the server will serve from
 # STATIC_ROOT = '/home/ubuntu/screentime_awareness_site/'
+# UPDATE: It is needed b/c I now use a 3rd party library (shared-calendar)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')
                     ]
