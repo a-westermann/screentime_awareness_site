9ee7e8cd388d8c9ca0cdc0bfc118ef94120e087f
Vierce1 <a.westermann.19@gmail.com>
2025-05-22 13:20:06 -0500

    smart bulb automation

--- screentime_awareness_site/settings.py
+++ screentime_awareness_site/settings.py
@@ -57,7 +57,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bootstrap5',
-    'shared_calendar'
+    'shared_calendar',
+    'govee_bulb_automation'
 ]
 
 MIDDLEWARE = [
